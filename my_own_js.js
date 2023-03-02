54dbdb261a18c5e0c9d6fe1d501fe516e9bd2df8
Kekilia87 <124105424+Kekilia87@users.noreply.github.com>
2023-03-01 22:50:30 -0300

    Entrega de proyecto
    Incluye tres formularios propios de html, css y js.

---
+++ my_own_js.js
@@ -0,0 +1,9 @@
+
+document.getElementById('boton1').onclick = function () {
+    document.getElementById('ayudita').style.display = 'block';
+}
+
+document.getElementById('boton2').onclick = function () {
+    alert("¡Gracias por responder! ¡Buena suerte! Y no olvides de enviar las respuestas")
+    console.log("Sí, se pudo")
+}
